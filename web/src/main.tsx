d6b92f7df9bce8689cd548c24f2f4d190c7658b3
jecmui <jessicamui90@gmail.com>
2025-11-08 18:02:25 -0500

    fix main page

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./index.css";
-import "./Home"
+import Home from "./Home";
 
 function App(){
   return (
